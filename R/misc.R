c62ac77113f54431d96e923701600b01498b766c
‘topepo’ <mxkuhn@gmail.com>
2024-04-15 15:50:36 -0400

    numeric_calibration

--- R/misc.R
+++ R/misc.R
@@ -5,3 +5,13 @@ is_tune <- function(x) {
   isTRUE(identical(quote(tune), x[[1]]))
 }
 
+# for operations with no tunable parameters
+
+no_param <-
+  tibble::tibble(
+    name = character(0),
+    call_info = list(),
+    source = character(0),
+    component = character(0),
+    component_id = character(0)
+  )
---
+++ R/numeric_calibration.R
@@ -0,0 +1,63 @@
+#' Re-calibrate numeric predictions
+#'
+#' @param x A [container()].
+#' @param calibrator A pre-trained calibration method from the \pkg{probably}
+#' package, such as [probably::cal_estimate_linear()].
+#' @export
+adjust_numeric_calibration <- function(x, calibrator) {
+
+  if ( !inherits(calibrator, "cal_regression") ) {
+    cli::cli_abort("The {.arg calibrator} argument should be an object of //
+                   class {.val 'cal_regression'}.")
+  }
+
+  op <-
+    new_operation(
+      "numeric_calibration",
+      inputs = "numeric",
+      outputs = "numeric",
+      arguments = list(calibrator = calibrator),
+      results = list(trained = FALSE)
+    )
+  x$operations <- c(x$operations, list(op))
+  x
+}
+
+#' @export
+print.numeric_calibration <- function(x, ...) {
+  trn <- ifelse(x$results$trained, " [trained]", "")
+  cli::cli_inform(c("Re-calibrate numeric predictions{trn}"))
+  invisible(x)
+}
+
+#' @export
+fit.numeric_calibration <- function(object, data, parent = NULL, ...) {
+  new_operation(
+    class(object),
+    inputs = object$inputs,
+    outputs = object$outputs,
+    arguments = object$arguments,
+    results = list(trained = TRUE)
+  )
+}
+
+#' @export
+predict.numeric_calibration <- function(object, new_data, parent, ...) {
+  probably::cal_apply(new_data, object$argument$calibrator)
+}
+
+# todo probably needs required_pkgs methods for cal objects
+#' @export
+required_pkgs.numeric_calibration <- function(x, ...) {
+  c("container", "probably")
+}
+
+#' @export
+tunable.numeric_calibration <- function (x, ...) {
+  no_param
+}
+
+# todo missing methods:
+# todo tune_args
+# todo tidy
+# todo extract_parameter_set_dials
