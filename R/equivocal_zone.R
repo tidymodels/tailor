00efa2aeedcb393a5ec6830b2a02d970b73beba0
‘topepo’ <mxkuhn@gmail.com>
2024-04-15 23:44:36 -0400

    add threshold

--- R/equivocal_zone.R
+++ R/equivocal_zone.R
@@ -3,6 +3,7 @@
 #' @param x A [container()].
 #' @param value A numeric value (between zero and 1/2) or [hardhat::tune()]. The
 #' value is the size of the buffer around the threshold.
+#' @param threshold A numeric value (between zero and one) or [hardhat::tune()].
 #' @examples
 #' library(dplyr)
 #' library(modeldata)
@@ -22,10 +23,13 @@
 #'
 #' predict(post_res, two_class_example)
 #' @export
-adjust_equivocal_zone <- function(x, value = 0.1) {
+adjust_equivocal_zone <- function(x, value = 0.1, threshold = 1 / 2) {
 
   if ( !is_tune(value) ) {
     check_number_decimal(value, min = 0, max = 1 / 2)
+  }
+  if ( !is_tune(threshold) ) {
+    check_number_decimal(threshold, min = 10^-10, max = 1 - 10^-10)
   }
 
   op <-
@@ -33,7 +37,7 @@ adjust_equivocal_zone <- function(x, value = 0.1) {
       "equivocal_zone",
       inputs = "probability",
       outputs = "class",
-      arguments = list(value = value),
+      arguments = list(value = value, threshold = threshold),
       results = list(trained = FALSE)
     )
   x$operations <- c(x$operations, list(op))
@@ -72,7 +76,8 @@ predict.equivocal_zone <- function(object, new_data, parent, ...) {
   lvls <- levels(new_data[[ est_nm ]])
   col_syms <- rlang::syms(prob_nm[1])
   cls_pred <- probably::make_two_class_pred(new_data[[prob_nm]], levels = lvls,
-                                            buffer = object$arguments$value)
+                                            buffer = object$arguments$value,
+                                            threshold = object$arguments$threshold)
   new_data[[ est_nm ]] <- cls_pred # todo convert to factor?
   new_data
 }
