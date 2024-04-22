17e154a6387f69cda4bc28a09733369ad29257da
simonpcouch <simonpatrickcouch@gmail.com>
2024-04-22 13:49:01 -0500

    align r/test files for workflow happiness

--- tests/testthat/_snaps/validation-rules.md
+++ tests/testthat/_snaps/validation-rules.md
@@ -1,4 +1,14 @@
-# validation of classification operations
+# validation of operations (regression)
+
+    Code
+      container(mode = "regression") %>% adjust_numeric_range(lower_limit = 2) %>%
+        adjust_numeric_calibration(dummy_reg_cal) %>% adjust_predictions_custom(
+        squared = .pred^2)
+    Condition
+      Error in `adjust_numeric_calibration()`:
+      ! Calibration should come before other operations.
+
+# validation of operations (classification)
 
     Code
       container(mode = "classification") %>% adjust_probability_threshold(threshold = 0.4) %>%
--- tests/testthat/test-validation-rules.R
+++ tests/testthat/test-validation-rules.R
@@ -1,5 +1,35 @@
-test_that("validation of classification operations", {
+test_that("validation of operations (regression)", {
+  dummy_reg_cal <- list()
+  class(dummy_reg_cal) <- "cal_regression"
 
+  expect_silent(
+    reg_ctr <-
+      container(mode = "regression") %>%
+      adjust_numeric_calibration(dummy_reg_cal) %>%
+      adjust_numeric_range(lower_limit = 2) %>%
+      adjust_predictions_custom(squared = .pred^2)
+  )
+
+  expect_snapshot(
+    container(mode = "regression") %>%
+      adjust_numeric_range(lower_limit = 2) %>%
+      adjust_numeric_calibration(dummy_reg_cal) %>%
+      adjust_predictions_custom(squared = .pred^2),
+    error = TRUE
+  )
+
+  # todo should we error if a mutate occurs beforehand? Can we detect if it
+  # modifies the prediction?
+  expect_silent(
+    reg_ctr <-
+      container(mode = "regression") %>%
+      adjust_predictions_custom(squared = .pred^2) %>%
+      adjust_numeric_calibration(dummy_reg_cal) %>%
+      adjust_numeric_range(lower_limit = 2)
+  )
+})
+
+test_that("validation of operations (classification)", {
   dummy_cls_cal <- list()
   class(dummy_cls_cal) <- "cal_binary"
 
