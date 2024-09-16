9760f87f77de3dcf225a4efaffc3ee1d992d764e
simonpcouch <simonpatrickcouch@gmail.com>
2024-09-16 09:56:59 -0400

    test basic `adjust_probability_calibration()` usage

--- tests/testthat/test-adjust-probability-calibration.R
+++ tests/testthat/test-adjust-probability-calibration.R
@@ -1,3 +1,49 @@
+test_that("basic adjust_probability_calibration() usage works", {
+  skip_if_not_installed("modeldata")
+  library(modeldata)
+
+  # split example data
+  set.seed(1)
+  in_rows <- sample(c(TRUE, FALSE), nrow(two_class_example), replace = TRUE)
+  d_potato <- two_class_example[in_rows, ]
+  d_test <- two_class_example[!in_rows, ]
+
+  # fitting and predicting happens without raising conditions
+  expect_no_condition(
+    tlr <-
+      tailor() %>%
+      adjust_probability_calibration(method = "logistic")
+  )
+
+  skip("TODO: cannot run for now due to #36")
+
+  expect_no_condition(
+    tlr_fit <- fit(
+      tlr,
+      d_potato,
+      outcome = c(truth),
+      estimate = c(predicted),
+      probabilities = c(Class1, Class2)
+    )
+  )
+
+  expect_no_condition(
+    predict(tlr_fit, d_test)
+  )
+
+  # classes are as expected
+  expect_s3_class(tlr, "tailor")
+  expect_s3_class(tlr_fit, "tailor")
+  expect_s3_class(tlr_pred, "tbl_df")
+
+  # column names are as expected
+  expect_equal(colnames(d_test), colnames(tlr_pred))
+
+  # calculations match those done manually
+  # TODO: write out the manual code with probably
+})
+
+
 test_that("adjustment printing", {
   expect_snapshot(tailor() %>% adjust_probability_calibration("logistic"))
 })
